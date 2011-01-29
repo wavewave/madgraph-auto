b5d95b96cf0e114a1dfd0397291f9531b6cb7892
ianwookim <ianwookim@gmail.com>
2011-01-29 07:25:18 +0000

    after 20110128 run

--- Model.hs
+++ Model.hs
@@ -14,35 +14,65 @@ data ModelVersion = MadGraph4 | MadGraph5
 
 data MassCoupParamSet = M600C3_65 | M600C4_6  | M400C3_6 
                       | M200C1_0  | M300C1_0  | M400C1_0  | M600C1_0
+                      | M400C3_45 | M400C3_3  | M400C3_15  
+                      | M600C4_4 | M600C4_2 | M600C4_0
+                      | M600C3_5  | M600C3_35 | M600C3_2
   
 massExotic :: MassCoupParamSet -> Double
-massExotic M600C3_65 = 600.0 
-massExotic M600C4_6  = 600.0
-massExotic M400C3_6  = 400.0 
-massExotic M200C1_0  = 200.0 
-massExotic M300C1_0  = 300.0 
-massExotic M400C1_0  = 400.0 
-massExotic M600C1_0  = 600.0 
+--massExotic M600C3_65 = 600.0 
+--massExotic M600C4_6  = 600.0
+--massExotic M400C3_6  = 400.0 
+--massExotic M200C1_0  = 200.0 
+--massExotic M300C1_0  = 300.0 
+--massExotic M400C1_0  = 400.0 
+--massExotic M600C1_0  = 600.0 
+massExotic M400C3_45 = 400.0 
+massExotic M400C3_3  = 400.0  
+massExotic M400C3_15 = 400.0   
+massExotic M600C4_4  = 600.0  
+massExotic M600C4_2  = 600.0 
+massExotic M600C4_0  = 600.0 
+massExotic M600C3_5  = 600.0 
+massExotic M600C3_35 = 600.0 
+massExotic M600C3_2  = 600.0
+
 
 coupExotic :: MassCoupParamSet -> Double 
-coupExotic M600C3_65 = 3.65 
-coupExotic M600C4_6  = 4.6
-coupExotic M400C3_6  = 3.6 
-coupExotic M200C1_0  = 1.0 
-coupExotic M300C1_0  = 1.0 
-coupExotic M400C1_0  = 1.0 
-coupExotic M600C1_0  = 1.0 
+--coupExotic M600C3_65 = 3.65 
+--coupExotic M600C4_6  = 4.6
+--coupExotic M400C3_6  = 3.6 
+--coupExotic M200C1_0  = 1.0 
+--coupExotic M300C1_0  = 1.0 
+--coupExotic M400C1_0  = 1.0 
+--coupExotic M600C1_0  = 1.0 
+coupExotic M400C3_45 = 3.45  
+coupExotic M400C3_3  = 3.3 
+coupExotic M400C3_15 = 3.15  
+coupExotic M600C4_4  = 4.4  
+coupExotic M600C4_2  = 4.2
+coupExotic M600C4_0  = 4.0 
+coupExotic M600C3_5  = 3.5 
+coupExotic M600C3_35 = 3.35
+coupExotic M600C3_2  = 3.2
 
 
-
-decayWidthExotic :: MassCoupParamSet -> Double  
-decayWidthExotic M600C3_65 = 133.32
-decayWidthExotic M600C4_6  = 211.75
-decayWidthExotic M400C3_6  = 67.685
-decayWidthExotic M200C1_0  = 0.23012
-decayWidthExotic M300C1_0  = 2.6191
-decayWidthExotic M400C1_0  = 5.2227
-decayWidthExotic M600C1_0  = 10.007
+decayWidthExotic :: Param -> Double  
+--decayWidthExotic M600C3_65 = 133.32
+--decayWidthExotic M600C4_6  = 211.75
+--decayWidthExotic M400C3_6  = 67.685
+--decayWidthExotic M200C1_0  = 0.23012
+--decayWidthExotic M300C1_0  = 2.6191
+--decayWidthExotic M400C1_0  = 5.2227
+--decayWidthExotic M600C1_0  = 10.007
+decayWidthExotic (TripParam M400C3_45) = 62.162
+decayWidthExotic (TripParam M400C3_3 ) = 56.875
+decayWidthExotic (TripParam M400C3_15) = 51.822
+decayWidthExotic (TripParam M600C4_4 ) = 193.73
+decayWidthExotic (TripParam M600C4_2 ) = 176.52
+decayWidthExotic (TripParam M600C4_0 ) = 160.11
+decayWidthExotic (SixParam  M600C3_5 ) = 122.59
+decayWidthExotic (SixParam  M600C3_35) = 112.30
+decayWidthExotic (SixParam  M600C3_2 ) = 102.47
 
 
 data Param = WpParam { massWp :: Double, gRWp :: Double } 
@@ -104,7 +134,7 @@ paramCardSetup tpath Six (SixParam set) = do
                templates
                [ ("massSix"      , (printf "%.4e" (massExotic set) :: String))
                , ("gRSix"        , (printf "%.4e" (coupExotic set) :: String))
-               , ("widthSix"      , (printf "%.4e" (decayWidthExotic set) :: String)) ]
+               , ("widthSix"      , (printf "%.4e" (decayWidthExotic (SixParam set)) :: String)) ]
                (paramCard4Model Six) ) ++ "\n\n\n"
 paramCardSetup tpath Trip (TripParam set) = do 
   templates <- directoryGroup tpath 
@@ -112,6 +142,6 @@ paramCardSetup tpath Trip (TripParam set) = do
                templates
                [ ("massTrip"      , (printf "%.4e" (massExotic set) :: String))
                , ("gRtrip"        , (printf "%.4e" (coupExotic set) :: String))
-               , ("widthTrip"      , (printf "%.4e" (decayWidthExotic set) :: String)) ]
+               , ("widthTrip"      , (printf "%.4e" (decayWidthExotic (TripParam set)) :: String)) ]
                (paramCard4Model Trip) ) ++ "\n\n\n"
 
--- madgraph20110128_benchmark2.hs
+++ madgraph20110128_benchmark2.hs
@@ -103,20 +103,54 @@ rsetup p matchtype num = RS {
   , setnum  = num
 }
 
-wpparamset = [ WpParam 200.0 0.85
-             , WpParam 300.0 1.20 
-             , WpParam 400.0 1.50
-             , WpParam 600.0 2.00 ] 
+wpparamset = [ WpParam 200.0 1.3
+             , WpParam 200.0 1.4
+             , WpParam 200.0 1.5
+             , WpParam 200.0 1.6
+             , WpParam 300.0 1.8
+             , WpParam 300.0 1.9
+             , WpParam 300.0 2.0
+             , WpParam 300.0 2.1
+             , WpParam 400.0 2.25
+             , WpParam 400.0 2.4
+             , WpParam 400.0 2.55
+             , WpParam 400.0 2.7
+             , WpParam 600.0 3.0
+             , WpParam 600.0 3.2
+             , WpParam 600.0 3.4
+             , WpParam 600.0 3.6 ]
+                         
+zpparamset = [ ZpHParam 200.0 0.90
+             , ZpHParam 200.0 0.95 
+             , ZpHParam 300.0 1.3 
+             , ZpHParam 300.0 1.2 
+             , ZpHParam 400.0 1.75 
+             , ZpHParam 400.0 1.65 
+             , ZpHParam 600.0 2.3 
+             , ZpHParam 600.0 2.2 ] 
 
-zpparamset = [ ZpHParam 200.0 0.70 
-             , ZpHParam 300.0 1.00 
-             , ZpHParam 400.0 1.30 
-             , ZpHParam 600.0 1.70 ] 
+
+{-
+wpparamset = [ WpParam 200.0 (0.85*sqrt 2)
+             , WpParam 300.0 (1.20*sqrt 2) 
+             , WpParam 400.0 (1.50*sqrt 2)
+             , WpParam 600.0 (2.00*sqrt 2) ] 
+
+zpparamset = [ ZpHParam 200.0 (0.70*sqrt 2) 
+             , ZpHParam 300.0 (1.00*sqrt 2) 
+             , ZpHParam 400.0 (1.30*sqrt 2) 
+             , ZpHParam 600.0 (1.70*sqrt 2) ] 
            
-tripparamset = [ TripParam M400C3_6
-               , TripParam M600C4_6 ] 
+tripparamset = [ TripParam M400C3_45
+               , TripParam M400C3_3 
+               , TripParam M400C3_15
+               , TripParam M600C4_4 
+               , TripParam M600C4_2 
+               , TripParam M600C4_0  ] 
              
-sixparamset = [ SixParam M600C3_65 ] 
+sixparamset = [ SixParam  M600C3_5   
+              , SixParam  M600C3_35 
+              , SixParam  M600C3_2  ]  -}
 
 psetuplist = [ psetup_wp_ttbar
              , psetup_zp_ttbar
@@ -127,7 +161,7 @@ psetuplist = [ psetup_wp_ttbar
              , psetup_trip_ttbar01j
              , psetup_six_ttbar01j ] 
 
-sets = [ 3..10 ] 
+sets = [1,2] -- [ 3..10 ] 
 
 
 wptasklist =  [ (psetup_wp_ttbar, rsetup p NoMatch num) | p <- wpparamset 
@@ -139,7 +173,7 @@ zptasklist =  [ (psetup_zp_ttbar, rsetup p NoMatch num) | p <- zpparamset
                                                         , num <- sets     ]
            ++ [ (psetup_zp_ttbar01j, rsetup p MLM num) | p <- zpparamset 
                                                            , num <- sets     ]
-                  
+{-                  
 triptasklist =  [ (psetup_trip_ttbar, rsetup p NoMatch num) | p <- tripparamset 
                                                             , num <- sets     ]
              ++ [ (psetup_trip_ttbar01j, rsetup p MLM num) | p <- tripparamset 
@@ -148,11 +182,11 @@ triptasklist =  [ (psetup_trip_ttbar, rsetup p NoMatch num) | p <- tripparamset
 sixtasklist =  [ (psetup_six_ttbar, rsetup p NoMatch num) | p <- sixparamset 
                                                           , num <- sets     ]
             ++ [ (psetup_six_ttbar01j, rsetup p MLM num) | p <- sixparamset 
-                                                             , num <- sets     ]
+                                                             , num <- sets     ] -}
 
-totaltasklist = wptasklist ++ zptasklist ++ triptasklist ++ sixtasklist
+totaltasklist = wptasklist ++ zptasklist {- ++ triptasklist ++ sixtasklist -}
 
-main = do putStrLn "benchmark models 20110126 sets" 
+main = do putStrLn "benchmark models 20110128 sets" 
           putStrLn "models : Wp, ZpH, Trip, Six "
           
 --          mapM_ (createWorkDir ssetup) psetuplist
