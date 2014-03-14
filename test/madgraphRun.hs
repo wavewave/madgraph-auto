1463909a633c13fc844636a16822353545adcd18
Ian-Woo Kim <ianwookim@gmail.com>
2014-03-14 22:55:29 +0100

    make test/madgraphRun.hs work with current version of madgraph-auto

--- test/madgraphRun.hs
+++ test/madgraphRun.hs
@@ -1,22 +1,21 @@
-{-# LANGUAGE PackageImports #-}
-
 module Main where
 
 import Control.Applicative
 import Control.Monad
-import "mtl" Control.Monad.Reader 
-import "mtl" Control.Monad.Error
+import Control.Monad.Reader 
+import Control.Monad.Error
 import System.FilePath 
 import System.Directory 
 import System.Log.Logger
 -- 
+-- import HEP.Automation.MadGraph.Card
 import HEP.Automation.MadGraph.Model
--- import HEP.Automation.MadGraph.Model.SM
-import HEP.Automation.MadGraph.Model.ADMXQLD211
-import HEP.Automation.MadGraph.Machine
-import HEP.Automation.MadGraph.UserCut
-import HEP.Automation.MadGraph.SetupType
+import HEP.Automation.MadGraph.Model.SM
+-- import HEP.Automation.MadGraph.Model.ADMXQLD211
 import HEP.Automation.MadGraph.Run
+import HEP.Automation.MadGraph.SetupType
+import HEP.Automation.MadGraph.Type
+import HEP.Parser.LHE.Sanitizer.Type
 import HEP.Storage.WebDAV
 -- 
 import qualified Paths_madgraph_auto as PMadGraph
@@ -31,146 +30,74 @@ getScriptSetup = do
   return $ 
     SS { modeltmpldir = mdldir 
        , runtmpldir = rundir 
-       , sandboxdir = homedir </> "repo/workspace/montecarlo/working"
-       , mg5base    = homedir </> "repo/ext/MadGraph5_v1_4_8_4/"
-       , mcrundir   = homedir </> "repo/workspace/montecarlo/mc/"
+       , sandboxdir = homedir </> "temp/montecarlo/sandbox"
+       , mg5base    = homedir </> "temp/montecarlo/MG5_aMC_v2_1_0"
+       , mcrundir   = homedir </> "temp/montecarlo/mcrun"
+       , pythia8dir = ""
+       , pythia8toHEPEVT = "" 
+       , hepevt2stdhep = "" 
        }
 
 -- | 
-processSetup :: ProcessSetup ADMXQLD211
+processSetup :: ProcessSetup SM
 processSetup = PS {  
-    model = ADMXQLD211
-  , process = "\n\
- \generate p p > go go / ul dl sl cl ur dr sr cr QED=0, (go > c~ cl, cl > d e+ sxxp~) , (go > c~ cl, cl > d e+ sxxp~ ) \n\
- \add process p p > go go / ul dl sl cl ur dr sr cr QED=0, (go > c~ cl, cl > d e+ sxxp~) , (go > c cl~, cl~ > d~ e- sxxp ) \n\
- \add process p p > go go / ul dl sl cl ur dr sr cr QED=0, (go > c cl~, cl~ > d~ e- sxxp) , (go > c~ cl, cl > d e+ sxxp~ ) \n\
- \add process p p > go go / ul dl sl cl ur dr sr cr QED=0, (go > c cl~, cl~ > d~ e- sxxp) , (go > c cl~, cl~ > d~ e- sxxp ) \n"
-
-    -- "\ngenerate P P > t1 t1~ QED=0, t1 > d e+ sxxp~ , t1~ > d~ e- sxxp \n"
-    -- "\ngenerate P P > t t~ \n" -- 
-  , processBrief = "gluinopair_stopdecayfull" 
-    -- "ttbar" -- 
-  , workname   = "Test22_20130221_ADMXQLD211"
+    model = SM
+  , process = MGProc [] ["p p > t t~"]
+  , processBrief = "ttbar" 
+  , workname   = "TestTTBar"
+  , hashSalt = HashSalt Nothing
   }
 
 -- | 
-psets :: [ModelParam ADMXQLD211]
-psets = [ ADMXQLD211Param { mstop = 50000, mgluino = x, msquark = y }
-        | (x,y) 
-            <- [(300,100)
-               ,(400,100),(400,200)
-               ,(500,100),(500,200),(500,300)
-               ,(600,100),(600,200),(600,300),(600,400)
-               ,(700,100),(700,200),(700,300),(700,400),(700,500)
-               ,(800,100),(800,200),(800,300),(800,400),(800,500),(800,600)
-               ,(900,100),(900,200),(900,300),(900,400),(900,500),(900,600),(900,700)
-               ,(1000,100),(1000,200),(1000,300),(1000,400),(1000,500),(1000,600),(1000,700),(1000,800) ] ] 
-
-
--- [200,300,400] ] 
-
--- x <- [100,500,1000,1500] ]
-
--- [500,1000] ] 
--- [1500] ] 
--- [100] ] 
--- [600,700,800] ]
-
--- [100,200..1600] ]
+pset :: ModelParam SM
+pset = SMParam
 
 -- | 
-ucut :: UserCut 
-ucut = UserCut { 
-    uc_metcut = 15.0
-  , uc_etacutlep = 2.7
-  , uc_etcutlep = 18.0 
-  , uc_etacutjet = 2.7
-  , uc_etcutjet = 15.0 
-}
+rsetup :: RunSetup
+rsetup = RS { numevent = 10000
+              , machine = LHC7 ATLAS
+              , rgrun   = Auto -- Fixed
+              , rgscale = 200.0
+              , match   = NoMatch
+              , cut     = NoCut 
+              , pythia  = NoPYTHIA -- RunPYTHIA
+              , lhesanitizer = [Replace [(9000201,1000022),(-9000201,1000022)]]
+              , pgs     = RunPGS (Cone 0.4, WithTau)
+              , uploadhep = NoUploadHEP
+              , setnum  = 1
+              } 
 
 -- | 
-rsetup p = RS { param = p
-            , numevent = 10000
-            , machine = LHC7 ATLAS
-            , rgrun   = Auto -- Fixed
-            , rgscale = 200.0
-            , match   = NoMatch
-            , cut     = NoCut 
-            , pythia  = RunPYTHIA
-            , usercut = NoUserCutDef 
-            , lhesanitizer = -- NoLHESanitize
-                             LHESanitize (Replace [(9000201,1000022),(-9000201,1000022)]) 
-                             -- LHESanitize (Elim [9000201]) 
-            , pgs     = RunPGS
-            , jetalgo = Cone 0.4
-            , uploadhep = NoUploadHEP
-            , setnum  = 1
-            }
+getWSetup :: IO (WorkSetup SM)
+getWSetup = WS <$> getScriptSetup <*> pure processSetup <*> pure pset <*> pure rsetup  
+               <*> pure (WebDAVRemoteDir "")
 
--- | 
-getWSetup :: [IO (WorkSetup ADMXQLD211)]
-getWSetup = [ WS <$> getScriptSetup <*> pure processSetup <*> pure (rsetup p) 
-                 <*> pure (CS NoParallel) 
-                 <*> pure (WebDAVRemoteDir "") | p <- psets ]
-
+main :: IO ()
 main = do 
   updateGlobalLogger "MadGraphAuto" (setLevel DEBUG)
-
-  mapM_ work =<< sequence getWSetup 
+  work =<< getWSetup 
 
 -- | 
--- work p  -- :: IO ()
-work wsetup = do -- wsetup <- getWSetup 
-            r <- flip runReaderT wsetup . runErrorT $ do 
-                 WS ssetup psetup rsetup _ _ <- ask 
-
-                 
-                 let wb = mcrundir ssetup 
-                     wn = workname psetup  
-                 b <- liftIO $ (doesDirectoryExist (wb </> wn))
-                 when (not b) $ createWorkDir ssetup psetup
-                 cardPrepare                      
-                 generateEvents   
-                 case (lhesanitizer rsetup,usercut rsetup,pythia rsetup) of
-                   (NoLHESanitize, NoUserCutDef,_) -> return ()
-                   (NoLHESanitize, UserCutDef _,_) -> do 
-                     runHEP2LHE       
-                     runHEPEVT2STDHEP 
-                     runPGS           
-                     runClean         
-                     updateBanner   
-                   (LHESanitize pid, NoUserCutDef, RunPYTHIA) -> do 
-                     sanitizeLHE
-                     runPYTHIA
-                     runHEP2LHE
-                     runPGS           
-                     runClean         
-                     updateBanner   
-                   (LHESanitize pid, NoUserCutDef, NoPYTHIA) -> do 
-                     sanitizeLHE
-                     updateBanner   
-                   (LHESanitize pid, UserCutDef _,RunPYTHIA) -> do 
-                     sanitizeLHE
-                     runPYTHIA
-                     runHEP2LHE       
-                     runHEPEVT2STDHEP 
-                     runPGS           
-                     runClean         
-                     updateBanner   
-                   (LHESanitize pid, UserCutDef _,NoPYTHIA) -> do 
-                     sanitizeLHE
-                     updateBanner    
-                 cleanHepFiles  
-            print r  
-            return ()
-
-
-
-
-          {-
-          -- create working directory (only once for each process)
-          mapM_ (createWorkDir my_ssetup) psetuplist
-          sleep 2
-          mapM_ (runReaderT cmdSequence) totaltasklist 
-          -}
-          
+work :: (WorkSetup SM) -> IO ()
+work wsetup = do 
+  r <- flip runReaderT wsetup . runErrorT $ do 
+       WS ssetup psetup _ rs _ <- ask                  
+       let wb = mcrundir ssetup 
+           wn = workname psetup  
+       b <- liftIO $ (doesDirectoryExist (wb </> wn))
+       when (not b) $ createWorkDir ssetup psetup
+       cardPrepare                      
+       generateEvents   
+       case (lhesanitizer rs, pythia rs) of
+         ([], _) -> return ()
+         (_:_, RunPYTHIA8) -> return ()
+         (_:_, RunPYTHIA) -> do 
+           sanitizeLHE
+           runPYTHIA
+           runPGS           
+           runClean         
+         (_:_, NoPYTHIA) -> do 
+           sanitizeLHE
+       cleanHepFiles  
+  print r  
+  return ()
