2622df89ea25366550f74a25302e8341705d0326
Ian-Woo Kim <ianwookim@gmail.com>
2015-06-09 01:35:56 +0200

    nixify

---
+++ default.nix
@@ -0,0 +1,17 @@
+{ mkDerivation, aeson, base, bytestring, devadmin, directory
+, filepath, hashable, hslogger, HStringTemplate, LHE-sanitizer, mtl
+, process, stdenv, text, transformers, unix, unordered-containers
+, webdav-manager
+}:
+mkDerivation {
+  pname = "madgraph-auto";
+  version = "0.999";
+  src = ./.;
+  buildDepends = [
+    aeson base bytestring devadmin directory filepath hashable hslogger
+    HStringTemplate LHE-sanitizer mtl process text transformers unix
+    unordered-containers webdav-manager
+  ];
+  description = "automated program library for madgraph run";
+  license = stdenv.lib.licenses.gpl3;
+}
